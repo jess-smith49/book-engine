e51483f6342103d9eb7972a126f2dff2b312f558
jess-smith49 <js.jessicasmith774@gmail.com>
2021-01-31 18:52:43 -0600

    started mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,14 @@ const {qgl} = require('apollo-server-express');
 
 const typeDefs = gql`
     type Query {
-        
+        me: [User]
+    }
+    
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+
+        removeBook(bookId: Id): User
     }`
    
 
