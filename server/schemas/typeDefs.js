03fce6863f6ef85271e3748f13e7596c725249aa
jess-smith49 <js.jessicasmith774@gmail.com>
2021-01-31 18:56:25 -0600

    most of the defTypes - working on parameter types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,6 +2,29 @@
 const {qgl} = require('apollo-server-express');
 
 const typeDefs = gql`
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: []
+    }
+
+    type Book{
+        bookId: ID
+        authors: []
+        description: String
+        title: String
+        image:
+        link:
+    }
+    
+    type Auth {
+        token:
+        user: [User]
+    }
+
     type Query {
         me: [User]
     }
